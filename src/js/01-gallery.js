7f506025005fdb501c30ecda1fdc25614bb5c18b
giligran <kuprykovrodion@gmail.com>
2023-05-27 16:01:06 +0300

    asa

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,60 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+SimpleLightbox
+const galleryContainer = document.querySelector('.gallery')
 
-console.log(galleryItems);
+function createItemsGalleryMarkup(galleryItems) {
+    const items = galleryItems
+        .map(
+            ({ original, preview, description }) =>
+                `<li class="gallery-item">
+                    <a class="gallery-link" href="${original}">
+                        <img 
+                        class="gallery-image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                        />
+                    </a>
+                </li>`
+        )
+        .join("");
+
+    return items;
+}
+
+galleryContainer.insertAdjacentHTML(
+    "beforeend",
+    createItemsGalleryMarkup(galleryItems)
+);
+
+const lightBox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    disableScroll: false,
+});
+
+lightBox.on("show.simplelightbox", () => {
+    const body = document.querySelector("body");
+    const bodyStyle = window.getComputedStyle(body);
+    const bodyWidth =
+        body.offsetWidth +
+        parseInt(bodyStyle.marginLeft) +
+        parseInt(bodyStyle.marginRight);
+    const verticalScrollBar = window.innerWidth - bodyWidth;
+
+    body.style.overflow = "hidden";
+    body.style.paddingRight = verticalScrollBar + "px";
+});
+
+lightBox.on("close.simplelightbox", () => {
+    const body = document.querySelector("body");
+
+    setTimeout(() => {
+        body.style.overflow = "auto";
+        body.style.paddingRight = "";
+    }, 250);
+})
