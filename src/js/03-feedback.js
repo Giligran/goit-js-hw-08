cbea0deced2585cb6483a229b2f31bbae25c74b2
Giligran <118477139+Giligran@users.noreply.github.com>
2023-05-28 16:09:50 +0300

    nnm

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,32 +7,32 @@ let formData = {};
 const formDataStr = localStorage.getItem(STORAGE_KEY);
 
 if (formDataStr) {
-    formData = JSON.parse(formDataStr);
-    formFeedback.email.value = formData.email;
-    formFeedback.message.value = formData.message;
+  formData = JSON.parse(formDataStr);
+  formFeedback.email.value = formData.email;
+  formFeedback.message.value = formData.message;
+}
+
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  if (formFeedback.email.value === '' || formFeedback.message.value === '') {
+    return;
   }
 
-  function onFormInput(e) {
-    formData[e.target.name] = e.target.value;
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-  }
+  formData.email = form.email.value;
+  formData.message = form.message.value;
 
-  function onFormSubmit(e) {
-    e.preventDefault();
-  
-    if (form.email.value === '' || form.message.value === '') {
-      return;
-    }
-  
-    formData.email = form.email.value;
-    formData.message = form.message.value;
-  
-    console.log(formData);
-  
-    formFeedback.reset();
-    localStorage.removeItem(STORAGE_KEY);
-    formData = {};
-  }
-  
-  formFeedback.addEventListener('input', throttle(onFormInput, 500));
-  formFeedback.addEventListener('submit', onFormSubmit);
+  console.log(formData);
+
+  formFeedback.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  formData = {};
+}
+
+formFeedback.addEventListener('input', throttle(onFormInput, 500));
+formFeedback.addEventListener('submit', onFormSubmit);
