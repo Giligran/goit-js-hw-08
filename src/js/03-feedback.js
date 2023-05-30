4b510cf0354e706a681ab0a8c6c0664cf80fb6b0
giligran <kuprykovrodion@gmail.com>
2023-05-30 12:57:22 +0300

    rewrite 03-feedbackform

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,44 +1,35 @@
 import throttle from 'lodash.throttle';
 
-const formFeedback = document.querySelector('.feedback-form');
+const form  = document.querySelector('.feedback-form');
 const STORAGE_KEY = 'feedback-form-state';
 
-let formData = {
-  email: '',
-  messsage: ''
-};
-const formDataStr = localStorage.getItem(STORAGE_KEY);
-
-if (formDataStr) {
-  formData = JSON.parse(formDataStr);
-  formFeedback.email.value = formData.email;
-  formFeedback.message.value = formData.message;
-}
-
-function onFormInput(e) {
-  formData[e.target.name] = e.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-}
-
-function onFormSubmit(e) {
-  e.preventDefault();
-
-  if (formFeedback.email.value === '' || formFeedback.message.value === '') {
-    return;
-  }
-
-  formData.email = formFeedback.email.value;
-  formData.message = formFeedback.message.value;
-
-  console.log(formData);
-
-  formFeedback.reset();
-  localStorage.removeItem(STORAGE_KEY);
-  formData = {
-    email: '',
-    messsage: ''
+console.log(form.querySelector('textarea[name="message"]'))
+console.log(form.message)
+console.log(form.email)
+form.addEventListener('input', throttle(() => {
+  const formData = {
+    email: form.email.value,
+    message: form.message.value,
   };
-}
+  localStorage.setItem("feedback-form-state", JSON.stringify(formData))
+}, 500))
 
-formFeedback.addEventListener('input', throttle(onFormInput, 500));
-formFeedback.addEventListener('submit', onFormSubmit);
+window.onload = function() {
+  localData = localStorage.getItem("feedback-form-state")
+  if (localData) {
+    const formData = JSON.parse(localData);
+    form.email.value = formData.email;
+    form.message.value = formData.message;
+  }  
+};
+
+form.addEventListener('submit', (e)=> {
+  e.preventDefault();
+  const formData = {
+    email: form.email.value,
+    message: form.message.value,
+  };
+  console.log(formData);
+  localStorage.removeItem("feedback-form-state")
+  form.reset();
+})
